052d20a4c4ac88412f8ec7f3c9bd6ae703fedb49
Maxx <maxx.v.garcia@gmail.com>
2018-09-19 19:55:47 -0600

    starting server

---
+++ dm-maps-server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser")
+require("dotenv").config();
+const expressJWT = require("express-jwt");
+const PORT = 8080;
+const MONGODB_URI = 'mongodb://localhost:27017/showtunes';
+const app = express();
+
+app.use(bodyParser.json())
+    .use("/user", expressJWT({ secret: process.env.SECRET }))
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use("/user/maps", require("./routes/maps"))
+    .use((err, req, res, next) => {
+        res.status(400).send(err)
+    });
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true})
+    .then((db) => console.log("DB is Online"))
+    .catch(err => console.log(err));
+    
+app.listen(PORT, () => console.log(`Connected to dm-maps-server on ${PORT}`));
